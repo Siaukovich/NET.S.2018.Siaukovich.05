41af79d2287e55ffa288db06fa3b18fcd0017cb7
Siaukovich Kiryl <k.sevkovich@gmail.com>
2018-07-07 20:30:47 +0300

    Tests for addition in Polynomial were added.

--- Polynomial.Tests/PolynomialTests.cs
+++ Polynomial.Tests/PolynomialTests.cs
@@ -1,12 +1,11 @@
 ﻿namespace Polynomial.Tests
 {
     using System;
-    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
 
     using NUnit.Framework;
 
     [TestFixture]
-    [SuppressMessage("ReSharper", "StyleCop.SA1600", Justification = "")]
     public class PolynomialTests
     {
         [TestCase(1.5, 2, 2, ExpectedResult = "1,5x^2 + 2x + 2")]
@@ -18,6 +17,23 @@
         {
             var p = new Polynomial(coeffs);
             return p.ToString();
+        }
+
+        [TestCase(new[] { 1d, 2, 3 }, new[] { 1d, 2, 3 }, new[] { 2d, 4, 6 })]
+        [TestCase(new[] { 0d, 2, 3 }, new[] { 1d, 2 }, new[] { 3d, 5 })]
+        [TestCase(new[] { 0d, 2, 0 }, new[] { -1d, 0 }, new[] { 1d, 0 })]
+        [TestCase(new[] { 0d, 3 }, new[] { 1.5, -12 }, new[] { 1.5, -9 })]
+        [TestCase(new[] { -5.25, 12, -43 }, new[] { 5.25, 0, 1 }, new[] { 0d, 12, -42 })]
+        public void Addition_ValieInput_ValidOutput(double[] d1, double[] d2, double[] sum)
+        {
+            var p1 = new Polynomial(d1);
+            var p2 = new Polynomial(d2);
+
+            var expected = new Polynomial(sum);
+
+            Polynomial actual = p1 + p2;
+
+            Assert.True(actual == expected);
         }
 
         [Test]
